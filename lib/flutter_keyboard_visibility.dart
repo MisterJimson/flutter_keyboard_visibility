d394d6d3df2ae23416bd9a3a65e9ec03dde580c5
Vince Ramces Oliveros <blusea231@gmail.com>
2020-06-22 19:54:26 +0800

    fix issues between keyboard and drawer (#29)

--- lib/flutter_keyboard_visibility.dart
+++ lib/flutter_keyboard_visibility.dart
@@ -171,8 +171,8 @@ class KeyboardDismissOnTap extends StatelessWidget {
   void _hideKeyboard(BuildContext context) {
     final currentFocus = FocusScope.of(context);
 
-    if (!currentFocus.hasPrimaryFocus) {
-      currentFocus.unfocus();
+    if (!currentFocus.hasPrimaryFocus && currentFocus.hasFocus) {
+      FocusManager.instance.primaryFocus.unfocus();
     }
   }
 
