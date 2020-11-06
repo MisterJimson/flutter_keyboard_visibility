f34ee7c4b31ffcac7dc1a1899032e137a8a819a6
Jason Rai <jrai@homex.com>
2020-11-06 15:53:07 -0500

    cleanup structure

---
+++ flutter_keyboard_visibility/lib/src/ui/keyboard_dismiss_on_tap.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/widgets.dart';
+
+/// Removes the current focus and hides the keyboard when
+/// the user taps on this widget.
+///
+/// Place this widget somewhere near the top of your widget
+/// tree and when the user taps outside of a focused widget,
+/// the focus will be removed and the keyboard will be hidden.
+class KeyboardDismissOnTap extends StatelessWidget {
+  const KeyboardDismissOnTap({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  final Widget child;
+
+  void _hideKeyboard(BuildContext context) {
+    final currentFocus = FocusScope.of(context);
+
+    if (!currentFocus.hasPrimaryFocus && currentFocus.hasFocus) {
+      FocusManager.instance.primaryFocus.unfocus();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        _hideKeyboard(context);
+      },
+      child: child,
+    );
+  }
+}
