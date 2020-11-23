e4d48735e7f45daeafbcacca88b9c3f431063079
Jason Rai <jrai@homex.com>
2020-11-23 08:49:58 -0500

    Documentation updates for web

--- flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
+++ flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
@@ -2,19 +2,22 @@ import 'dart:html' as html show window, Navigator;
 import 'package:flutter_keyboard_visibility_platform_interface/flutter_keyboard_visibility_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-/// The web implementation of the [FlutterKeyboardVisibilityPlatform] of the FlutterKeyboardVisibility plugin.
+/// The web implementation of the [FlutterKeyboardVisibilityPlatform] of the
+/// FlutterKeyboardVisibility plugin.
 class FlutterKeyboardVisibilityPlugin
     extends FlutterKeyboardVisibilityPlatform {
   /// Constructs a [FlutterKeyboardVisibilityPlugin].
   FlutterKeyboardVisibilityPlugin(html.Navigator navigator);
 
-  /// Factory method that initializes the FlutterKeyboardVisibility plugin platform with an instance
-  /// of the plugin for the web.
+  /// Factory method that initializes the FlutterKeyboardVisibility plugin
+  /// platform with an instance of the plugin for the web.
   static void registerWith(Registrar registrar) {
     FlutterKeyboardVisibilityPlatform.instance =
         FlutterKeyboardVisibilityPlugin(html.window.navigator);
   }
 
+  /// Emits changes to keyboard visibility from the platform. Web is not
+  /// implemented yet so false is returned.
   @override
   Stream<bool> get onChange async* {
     yield false;
