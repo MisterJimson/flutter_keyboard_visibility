7b8c976a996c2ba448a2c41d306bef63065e997a
Jason Rai <jrai@homex.com>
2020-11-23 08:20:36 -0500

    publish web

--- flutter_keyboard_visibility_web/CHANGELOG.md
+++ flutter_keyboard_visibility_web/CHANGELOG.md
@@ -1,3 +1,3 @@
-## [1.0.0] - November 11, 2020
+## [1.0.0] - November 23, 2020
 
 * Initial support so Flutter apps that run on web won't encounter errors. Visibility is returned as false.
--- flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
+++ flutter_keyboard_visibility_web/lib/flutter_keyboard_visibility_web.dart
@@ -2,13 +2,13 @@ import 'dart:html' as html show window, Navigator;
 import 'package:flutter_keyboard_visibility_platform_interface/flutter_keyboard_visibility_platform_interface.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
-/// The web implementation of the [FlutterKeyboardVisibilityPlatform] of the Battery plugin.
+/// The web implementation of the [FlutterKeyboardVisibilityPlatform] of the FlutterKeyboardVisibility plugin.
 class FlutterKeyboardVisibilityPlugin
     extends FlutterKeyboardVisibilityPlatform {
   /// Constructs a [FlutterKeyboardVisibilityPlugin].
   FlutterKeyboardVisibilityPlugin(html.Navigator navigator);
 
-  /// Factory method that initializes the Battery plugin platform with an instance
+  /// Factory method that initializes the FlutterKeyboardVisibility plugin platform with an instance
   /// of the plugin for the web.
   static void registerWith(Registrar registrar) {
     FlutterKeyboardVisibilityPlatform.instance =
