5ed2deb9db04db15d610097c50c950d109a4bf67
Jason Rai <jason.c.rai@gmail.com>
2021-07-20 08:43:00 -0400

    chore: update deps so pub get works on stable (#80)

--- pub_get.sh
+++ pub_get.sh
@@ -2,3 +2,4 @@
 (cd flutter_keyboard_visibility_platform_interface && flutter pub get)
 (cd flutter_keyboard_visibility_web && flutter pub get)
 (cd flutter_keyboard_visibility && flutter pub get)
+(cd flutter_keyboard_visibility/example_old && flutter pub get)
