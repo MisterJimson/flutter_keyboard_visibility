a10c1e6e642da63bdb7928f98b4e31f04c4e3fbe
Fabian Quintanar <croxx5f@protonmail.com>
2021-08-29 17:21:58 -0400

    Add drag dismissal to `KeyboardDismissOnTap`

--- flutter_keyboard_visibility/lib/src/ui/keyboard_dismiss_on_tap.dart
+++ flutter_keyboard_visibility/lib/src/ui/keyboard_dismiss_on_tap.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/widgets.dart';
 
 /// Removes the current focus and hides the keyboard when
-/// the user taps on this widget.
+/// the user drags or taps this widget.
 ///
 /// Place this widget somewhere near the top of your widget
-/// tree and when the user taps outside of a focused widget,
+/// tree and when the user drags or taps outside of a focused widget,
 /// the focus will be removed and the keyboard will be hidden.
 class KeyboardDismissOnTap extends StatelessWidget {
+  final Widget child;
+  final bool dismissOnTap;
+  final bool dismissOnDrag;
+
   const KeyboardDismissOnTap({
     Key? key,
     required this.child,
+    this.dismissOnTap = true,
+    this.dismissOnDrag = false,
   }) : super(key: key);
-
-  final Widget child;
 
   void _hideKeyboard(BuildContext context) {
     final currentFocus = FocusScope.of(context);
@@ -24,11 +28,18 @@ class KeyboardDismissOnTap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        _hideKeyboard(context);
+    return NotificationListener<ScrollUpdateNotification>(
+      onNotification: (ScrollUpdateNotification notification) {
+        final focusScope = FocusScope.of(context);
+        if (dismissOnDrag && notification.dragDetails != null && focusScope.hasFocus) {
+          focusScope.unfocus();
+        }
+        return false;
       },
-      child: child,
+      child: GestureDetector(
+        onTap: dismissOnTap ? () => _hideKeyboard(context) : null,
+        child: child,
+      ),
     );
   }
 }
