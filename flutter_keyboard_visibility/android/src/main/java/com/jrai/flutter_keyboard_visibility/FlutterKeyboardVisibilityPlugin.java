96cc5bf41c6c432d3537850fac4ea102f0152387
Jason Rai <jason.c.rai@gmail.com>
2021-10-15 08:30:30 -0400

    Remove old android support (#97)
    * remove old example
    
    * update Android example
    
    * remove registerWith
    
    * version and changelog

--- flutter_keyboard_visibility/android/src/main/java/com/jrai/flutter_keyboard_visibility/FlutterKeyboardVisibilityPlugin.java
+++ flutter_keyboard_visibility/android/src/main/java/com/jrai/flutter_keyboard_visibility/FlutterKeyboardVisibilityPlugin.java
@@ -24,21 +24,6 @@ public class FlutterKeyboardVisibilityPlugin implements FlutterPlugin, ActivityA
     init(flutterPluginBinding.getBinaryMessenger());
   }
 
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(PluginRegistry.Registrar registrar) {
-    final FlutterKeyboardVisibilityPlugin plugin = new FlutterKeyboardVisibilityPlugin();
-    plugin.init(registrar.messenger());
-    plugin.listenForKeyboard(registrar.activity());
-  }
-
   private void init(BinaryMessenger messenger) {
     final EventChannel eventChannel = new EventChannel(messenger, "flutter_keyboard_visibility");
     eventChannel.setStreamHandler(this);
