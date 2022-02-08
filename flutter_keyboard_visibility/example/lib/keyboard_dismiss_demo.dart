b71ba397217d343a532d66c542cf2bec1654e96e
Jason Rai <jrai@homex.com>
2022-02-08 09:05:06 -0500

    update example app for testing

---
+++ flutter_keyboard_visibility/example/lib/keyboard_dismiss_demo.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_keyboard_visibility/flutter_keyboard_visibility.dart';
+
+class KeyboardDismissDemo extends StatelessWidget {
+  const KeyboardDismissDemo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Keyboard Dismiss Demo'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              KeyboardDismissOnTap(
+                child: Container(
+                  height: 80,
+                  width: double.infinity,
+                  color: Colors.red,
+                  child: Center(child: Text('Red dismisses on tap')),
+                ),
+              ),
+              KeyboardDismissOnTap(
+                child: Container(
+                  height: 80,
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Button does not dismiss, blue does'),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Button'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              KeyboardDismissOnTap(
+                ignoreGestureDetectors: false,
+                child: Container(
+                  height: 80,
+                  width: double.infinity,
+                  color: Colors.green,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Button and green both dismiss'),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text('Button'),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Spacer(),
+              TextField(
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  labelText: 'Input box for keyboard test',
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
