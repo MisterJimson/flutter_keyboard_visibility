49210bd71e3ae3b912ae803763c6088295406d97
Jason Rai <me@jasonrai.ca>
2022-11-16 13:00:00 -0500

    chore: changes after flutter upgrade & pub get

--- flutter_keyboard_visibility/example/windows/flutter/generated_plugin_registrant.cc
+++ flutter_keyboard_visibility/example/windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,6 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <flutter_keyboard_visibility_windows/flutter_keyboard_visibility_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  FlutterKeyboardVisibilityPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("FlutterKeyboardVisibilityPlugin"));
 }
